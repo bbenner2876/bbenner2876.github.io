8da235bd07b33cddfd2cf8b7ed8a90df386a2676
bbenner2876 <bennerbrett2876@gmail.com>
2017-11-13 21:24:14 -0800

    Added links, files, and styling to Projects.

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,20 @@ function hamburgerToggle() { /*JavaScript hamburger code taken from w3schools.co
     }
 }
 
-function popUp(){
-    alert("Popped up!");
+/*Modal function ---- not used*/
+var modal = document.getElementById('myModal');
+
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+    modal.style.display = "none";
 }
